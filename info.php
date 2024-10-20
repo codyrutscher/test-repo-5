2eb6eb74b97a7c43ceb6874ea7850fa273297944
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-10-20 07:41:20 -0500

    Update info.php

--- info.php
+++ info.php
@@ -17,10 +17,10 @@
           <svg class="shrink-0 size-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
         </div>
         <div class="shrink-0">
-          <h3 class="block text-lg font-semibold text-gray-800 dark:text-white">Build your portfolio</h3>
+          <h3 class="block text-lg font-semibold text-gray-800 dark:text-white">GitLocker</h3>
         </div>
       </div>
-      <p class="text-gray-600 dark:text-neutral-400">The simplest way to keep your portfolio always up-to-date.</p>
+      <a href="https://www.gitlocker.com"><p class="text-gray-600 dark:text-neutral-400">The simplest way to keep your portfolio always up-to-date.</p></a>
     </div>
     <!-- End Card -->
 
